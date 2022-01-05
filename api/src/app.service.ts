7aea848182e7c579b260ac1a71a9b8baa28f5fcb
Abderrahmane Gourragui <gourragui.success@gmail.com>
2022-01-06 00:56:51 +0100

    initial commit

---
+++ api/src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'ha l9wada jaya, chakhsiyan';
+  }
+}
---
+++ web/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Snooker Scoreboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p>scoreboard dyal snooker</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
