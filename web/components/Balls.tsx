a3645e68b28e964c17a21c98288c685fefed6c9f
Elliop <abdelmouhayminegx@gmail.com>
2022-01-07 15:40:44 +0100

    Add Balls buttons

--- web/components/Balls.tsx
+++ web/components/Balls.tsx
@@ -1,10 +1,16 @@
+import { RefreshIcon } from '@heroicons/react/outline';
+
 const Balls = () => {
   return (
-    <div
-      className="mx-10 flex justify-center items-center border-[1px] 
-        border-primary-w bg-primary-b rounded-lg px-8 py-3 my-8"
-    >
-      <h1 className="text-primary-w font-semibold text-xl">Balls</h1>
+    <div className="flex justify-between items-center px-8 py-3 my-8 mx-20">
+      <button className="w-14 h-14 rounded-full bg-red-800"></button>
+      <button className="w-14 h-14 rounded-full bg-yellow-400"></button>
+      <button className="w-14 h-14 rounded-full bg-green-600"></button>
+      <button className="w-14 h-14 rounded-full bg-amber-900"></button>
+      <button className="w-14 h-14 rounded-full bg-blue-600"></button>
+      <button className="w-14 h-14 rounded-full bg-pink-300"></button>
+      <button className="w-14 h-14 rounded-full bg-black"></button>
+      <RefreshIcon className="w-14 h-14 text-white cursor-pointer" />
     </div>
   );
 };
--- web/components/History.tsx
+++ web/components/History.tsx
@@ -4,7 +4,7 @@ const History = () => {
       className="mr-10 flex justify-center items-center border-[1px] 
         border-primary-w bg-primary-b rounded-lg px-8 py-3 my-8"
     >
-      <h1 className="text-primary-w font-semibold text-xl">Historique</h1>
+      <h1 className="text-primary-w font-semibold text-3xl">Historique</h1>
     </div>
   );
 };
