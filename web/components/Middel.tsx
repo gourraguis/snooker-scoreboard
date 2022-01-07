47661331a74afdef72869da00ce67842abdfc0b6
Elliop <abdelmouhayminegx@gmail.com>
2022-01-07 14:29:35 +0100

    Add Timer to middel section

--- web/components/Middel.tsx
+++ web/components/Middel.tsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from 'react';
+
 const Middel = () => {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [startTimer, setStartTimer] = useState(true);
+
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (startTimer) setSeconds(seconds + 1);
+      if (seconds === 59) {
+        setMinutes(minutes + 1);
+        setSeconds(0);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
   return (
     <div
-      className="flex justify-center items-center border-[1px] 
-        border-primary-w bg-primary-b rounded-lg px-8 py-3 my-8"
+      className="grid grid-row-3 border-[1px] 
+        border-primary-w bg-primary-b rounded-lg
+        my-8 divide-y divide-primary-w
+        "
     >
-      <h1 className="text-primary-w font-semibold text-xl">0 0 : 2 8</h1>
+      <div className="w-full flex justify-center items-center">
+        <h1 className="text-primary-w font-semibold text-5xl py-4">
+          {minutes} : {seconds}
+        </h1>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <h1 className="text-primary-w font-semibold text-4xl py-4">LAST 7</h1>
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <h1 className="text-primary-w font-semibold text-4xl py-4">SCORE 8</h1>
+      </div>
     </div>
   );
 };
