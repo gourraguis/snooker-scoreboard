d06ea1dc6bac0abc9360da70599758ea85c98f7d
Elliop <abdelmouhayminegx@gmail.com>
2022-01-07 13:43:31 +0100

    Finish Player component

--- web/components/Player.tsx
+++ web/components/Player.tsx
@@ -2,26 +2,26 @@ import { UserIcon } from '@heroicons/react/solid';
 import { useRecoilState } from 'recoil';
 import { selectedUserState } from '../atoms/selectedUserState';
 
-const Player = ({ player }: any) => {
+const Player = ({ id, color, score }: any) => {
   const [selectedUser, setSelectedUser] = useRecoilState(selectedUserState);
   const handleUser = () => {
-    setSelectedUser(player);
+    setSelectedUser(id);
   };
   return (
     <div
       onClick={handleUser}
       className={`flex flex-col justify-center items-center border-[1px] 
         border-primary-w bg-primary-b rounded-lg py-1
-        my-8 divide-y divide-primary-w ${
-          selectedUser === player &&
+        my-8 divide-y divide-primary-w cursor-pointer ${
+          selectedUser === id &&
           'divide-green-600 border-green-600 shadow-md shadow-green-600 '
         }"`}
     >
       <div className="w-full flex justify-center items-center">
-        <UserIcon className="w-20 h-20 text-red-800 my-2" />
+        <UserIcon className={`w-20 h-20 my-2 ${color}`} />
       </div>
       <div className="w-full flex justify-center items-center">
-        <h1 className="text-primary-w font-semibold text-4xl py-4">36</h1>
+        <h1 className="text-primary-w font-semibold text-4xl py-4">{score}</h1>
       </div>
     </div>
   );
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,6 +6,24 @@ import History from '../components/History';
 import Middel from '../components/Middel';
 import Player from '../components/Player';
 
+interface player {
+  id: String;
+  color: String;
+  score: number;
+}
+
+const player1: player = {
+  id: '1',
+  color: 'text-red-800',
+  score: 36,
+};
+
+const player2: player = {
+  id: '2',
+  color: 'text-blue-800',
+  score: 54,
+};
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -20,8 +38,16 @@ const Home: NextPage = () => {
         <main className="flex flex-col justify-center">
           <div className="grid grid-cols-3 gap-28">
             <div className="ml-10">
-              <Player player="1" />
-              <Player player="2" />
+              <Player
+                id={player1.id}
+                score={player1.score}
+                color={player1.color}
+              />
+              <Player
+                id={player2.id}
+                score={player2.score}
+                color={player2.color}
+              />
             </div>
             <Middel />
             <History />
