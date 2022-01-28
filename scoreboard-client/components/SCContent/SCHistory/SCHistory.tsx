1f62423bf4cd02a245f59a05d2ea28d48bc0dcfd
Elliop <abdelmouhayminegx@gmail.com>
2022-01-28 17:16:28 +0100

    Add animations

--- scoreboard-client/components/SCContent/SCHistory/SCHistory.tsx
+++ scoreboard-client/components/SCContent/SCHistory/SCHistory.tsx
@@ -1,4 +1,5 @@
 import { UserOutlined } from '@ant-design/icons'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useRecoilValue } from 'recoil'
 import { Card } from 'antd'
 import { Content } from 'antd/lib/layout/layout'
@@ -17,17 +18,27 @@ const SCHistory = () => {
     <Card title={<h3 className={styles.title}>Historique</h3>} className={styles.card}>
       <Content className={styles.content}>
         {shownHistory.map((item, index) => (
-          <div key={index} className={styles.wrapper}>
-            <UserOutlined className={styles[`icon${item.value}`]} />
-            <div>
-              <h3 className={styles.text}>Marque {item.scoredBalls.reduce((a, b) => a + b, 0)} points</h3>
-              <div className={styles.ballBox}>
-                {item.scoredBalls.map((ball, index2) => (
-                  <SCBall key={index2} value={ball} size="sm" />
-                ))}
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              key={item.value}
+              animate={{ y: 0 }}
+              initial={{ y: 20 }}
+              exit={{ y: -20 }}
+              transition={{ duration: 0.15 }}
+            >
+              <div key={index} className={styles.wrapper}>
+                <UserOutlined className={styles[`icon${item.value}`]} />
+                <div>
+                  <p className={styles.text}>Marque {item.scoredBalls.reduce((a, b) => a + b, 0)} points</p>
+                  <div className={styles.ballBox}>
+                    {item.scoredBalls.map((ball, index2) => (
+                      <SCBall key={index2} value={ball} size="sm" />
+                    ))}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </motion.div>
+          </AnimatePresence>
         ))}
       </Content>
     </Card>
