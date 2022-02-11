ca38669169855b3e59cd459b0c990521eea3c979
Elliop <abdelmouhayminegx@gmail.com>
2022-02-11 17:42:35 +0100

    replace login modal with 2 steps form

--- owner-dashboard/components/ODLogin/ODLogin.tsx
+++ owner-dashboard/components/ODLogin/ODLogin.tsx
@@ -1,47 +1,79 @@
+import { useRouter } from 'next/router'
 import { FunctionComponent } from 'react'
 import { Form, Input, Button, Layout, Card } from 'antd'
 
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useSetRecoilState } from 'recoil'
 import styles from './ODLogin.module.css'
 import { ILogin } from '../../types/login'
-import { loginOwner } from '../../services/owner'
-import { otpModalState } from '../../atoms/authState'
-import ODOtpModal from './ODOtpModal'
+import { checkOtp, loginOwner } from '../../services/owner'
+import { authState, otpModalState } from '../../atoms/authState'
 
 const { Content } = Layout
 
 export const ODLogin: FunctionComponent = () => {
-  const [isModalVisible, setIsModalVisible] = useRecoilState(otpModalState)
+  const [otpVerif, setOtpVerif] = useRecoilState(otpModalState)
 
   const handleCancel = () => {
-    setIsModalVisible(false)
+    setOtpVerif(false)
   }
   const onFinish = (values: ILogin) => {
-    loginOwner(values, setIsModalVisible)
+    loginOwner(values, setOtpVerif)
+  }
+
+  const router = useRouter()
+  const setAuth = useSetRecoilState(authState)
+
+  const onFinishOtp = (values: { code: string }) => {
+    checkOtp(values.code, setAuth, router)
   }
 
   return (
     <Content className={styles.content}>
-      <Card title="Login Form" bordered={false} style={{ width: 300 }}>
-        <Form
-          name="login"
-          wrapperCol={{ span: 24 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete="off"
-        >
-          <Form.Item name="phoneNumber" rules={[{ required: true, message: 'Please input your phone number!' }]}>
-            <Input placeholder="Phone Number!" />
-          </Form.Item>
+      {!otpVerif && (
+        <Card title="Login Form" bordered={false} style={{ width: 300 }}>
+          <Form
+            name="login"
+            wrapperCol={{ span: 24 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            autoComplete="off"
+          >
+            <Form.Item name="phoneNumber" rules={[{ required: true, message: 'Please input your phone number!' }]}>
+              <Input placeholder="Phone Number!" />
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ span: 24 }}>
-            <Button type="primary" htmlType="submit">
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-      {isModalVisible && <ODOtpModal onCancel={handleCancel} visible={isModalVisible} />}
+            <Form.Item wrapperCol={{ span: 24 }}>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      )}
+      {otpVerif && (
+        <Card title="Veuillez insérer le code" bordered={false} style={{ width: 300 }}>
+          <Form
+            name="login"
+            wrapperCol={{ span: 22 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinishOtp}
+            autoComplete="off"
+          >
+            <Form.Item name="code">
+              <Input placeholder="Code!" />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ span: 24 }}>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+              <Button type="primary" style={{ margin: '0 29px' }} onClick={handleCancel}>
+                Chnage phone
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      )}
     </Content>
   )
 }
