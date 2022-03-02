49aec6a5d69ac8e96e25bc1d9dd51c1d50f32da5
Elliop <abdelmouhayminegx@gmail.com>
2022-03-02 12:10:11 +0100

    add emiter for new player name

--- api/src/app/socket-emitters/board-emitter.gateway.ts
+++ api/src/app/socket-emitters/board-emitter.gateway.ts
@@ -2,6 +2,7 @@ import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
 import { BoardService } from '../board/board.service'
 import { IGame } from '../game/types/game'
 import { BoardServer } from '../types/board-sockets'
+import { IInitBoard } from '../types/initBoard'
 
 @WebSocketGateway({ cors: true, namespace: 'board' })
 export class BoardEmitterGateway {
@@ -13,5 +14,10 @@ export class BoardEmitterGateway {
   public async emitStartNewGame(newGame: IGame) {
     const board = await this.boardService.getBoard(newGame.id)
     this.server.to(board.socketId).emit('initGame', newGame)
+  }
+
+  public async emitUpdatePlayerName(updatedGame: IInitBoard) {
+    const board = await this.boardService.getBoard(updatedGame.boardId)
+    this.server.to(board.socketId).emit('updatePlayerName', updatedGame)
   }
 }
