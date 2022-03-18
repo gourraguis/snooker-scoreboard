0359016a050d12f49c65432c4966d639b239575e
Elliop <abdelmouhayminegx@gmail.com>
2022-03-18 13:09:33 +0100

    finish manager section

--- website/components/WSManager/WSManager.tsx
+++ website/components/WSManager/WSManager.tsx
@@ -11,7 +11,9 @@ export const WSManager: FunctionComponent = () => (
     </div>
     <div className={styles.wrapText}>
       <h2 className={styles.header}>Manager dashboard</h2>
-      <p className={styles.text}>Espace du serveur pour pouvoir lancer et manipuler les parties facilement.</p>
+      <div className={styles.wrapP}>
+        <p className={styles.text}>Espace du serveur pour pouvoir lancer et manipuler les parties facilement.</p>
+      </div>
       <div className={styles.buttonBos}>
         <Button className={styles.button} type="primary">
           Login
