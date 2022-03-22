ed2ea67ea4d9c8e9f1f43010c3fe9d410ea206f1
Abderrahmane <gourragui.success@gmail.com>
2022-03-22 23:46:35 +0100

    implement browser for scoreboard

--- browser-scoreboard/index.js
+++ browser-scoreboard/index.js
@@ -1,10 +1,21 @@
+require('dotenv').config({ path: 'config.txt' })
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
-
-  await browser.close();
+  const boardId = process.env.BOARD_ID
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args:[
+      '--start-fullscreen',
+      '--no-default-browser-check',
+      '--disable-infobars',
+      '--test-type=webdriver'
+   ],
+   ignoreDefaultArgs: ['--enable-automation']
+  });
+  const pages = await browser.pages();
+  const page = pages[0];
+  // const page = await browser.newPage();
+  await page.goto(`https://client.jawad.club/?id=${boardId}`);
 })();
