b7c1bf78ed6f7c7ace3e73277661ecd60702c2a4
Elliop <58885203+Elliop@users.noreply.github.com>
2022-03-25 13:03:34 +0100

    show data from db

--- owner-dashboard/components/ODStatistics/ODSelector/ODSelector.tsx
+++ owner-dashboard/components/ODStatistics/ODSelector/ODSelector.tsx
@@ -1,8 +1,9 @@
 import { Button, Select, DatePicker, Form } from 'antd'
 import { FunctionComponent, useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import { managersStats, tablesStats } from '../../../atoms/mainStats'
 import styles from './ODSelector.module.css'
 
-const { Option } = Select
 const { RangePicker } = DatePicker
 
 const rangeConfig = {
@@ -12,41 +13,38 @@ const rangeConfig = {
 export const ODSelector: FunctionComponent = () => {
   const [selectedManager, setSelectedManager] = useState()
   const [selectedTable, setSelectedTable] = useState()
+  const tablesElements = useRecoilValue(tablesStats)
+  const managersElements = useRecoilValue(managersStats)
 
   const onFinish = (fieldsValue: { [x: string]: any }) => {
     const rangeValue = fieldsValue['range-picker']
     const values = {
-      manager: selectedManager,
-      table: selectedTable,
+      managerId: selectedManager,
+      tableId: selectedTable,
       startDate: rangeValue[0].format('YYYY-MM-DD'),
       endDate: rangeValue[1].format('YYYY-MM-DD'),
     }
-    console.log('Received values of form: ', values)
+    console.log(values)
   }
 
   return (
     <Form className={styles.all} name="time_related_controls" onFinish={onFinish}>
-      <Select
-        className={styles.item}
-        showSearch
-        placeholder="Select Manager"
-        optionFilterProp="children"
-        onChange={(value) => setSelectedManager(value)}
-      >
-        <Option value="jack">Jack</Option>
-        <Option value="lucy">Lucy</Option>
-        <Option value="tom">Tom</Option>
-      </Select>
+      <Form.Item className={styles.item}>
+        <Select showSearch placeholder="Select Manager" onChange={(value) => setSelectedManager(value)}>
+          {managersElements.map((elem) => (
+            <Select.Option value={elem.id}>{elem.name}</Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
       <Form.Item className={styles.item}>
         <Select showSearch placeholder="Select Table" onChange={(value) => setSelectedTable(value)}>
-          <Select.Option value="table1">table 1</Select.Option>
-          <Select.Option value="table2">table 2</Select.Option>
-          <Select.Option value="table3">table 3</Select.Option>
-          <Select.Option value="table4">table 4</Select.Option>
+          {tablesElements.map((elem) => (
+            <Select.Option value={elem.id}>{elem.name}</Select.Option>
+          ))}
         </Select>
       </Form.Item>
       <Form.Item className={styles.item} name="range-picker" {...rangeConfig}>
-        <RangePicker className={styles.item} />
+        <RangePicker className={styles.date} />
       </Form.Item>
       <Button className={styles.button} type="primary" htmlType="submit">
         Search
